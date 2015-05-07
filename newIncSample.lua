841980a84c565ce5a9d45a2e9f80bac8fa9247b2
hpljack <258146182@qq.com>
2015-05-07 16:42:01 +0800

    modified

--- newIncSample.lua
+++ newIncSample.lua
@@ -1,8 +1,11 @@
 function calcStep(initNo)
+	sum = 0;
 	for i = 1, 6,1 do
+		sum = initNo + sum
 		print(i..": "..initNo)
 		initNo = initNo * 3
 	end
+	print("sum of money: "..sum)
 end
 
 calcStep(arg[1])
