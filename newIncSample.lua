31a1286ebcab65ef7583b25280243282736fcc8e
hpljack <258146182@qq.com>
2015-05-07 16:55:32 +0800

    add default initNo

--- newIncSample.lua
+++ newIncSample.lua
@@ -2,10 +2,14 @@ function calcStep(initNo)
 	sum = 0;
 	for i = 1, 6,1 do
 		sum = initNo + sum
-		print(i..": "..initNo)
+		print(i..": "..initNo.." calc: "..sum)
 		initNo = initNo * 3
 	end
 	print("sum of money: "..sum)
+end
+
+if arg[1] == nil then
+	arg[1] = 5
 end
 
 calcStep(arg[1])
