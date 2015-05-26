c9b5f0985ed08034726a7e01732606d2ff1f9324
hpljack <258146182@qq.com>
2015-05-26 17:28:24 +0800

    add the lastthreeinc.lua

---
+++ lastthreeinc.lua
@@ -0,0 +1,18 @@
+function calcStep(initNo)
+	sum = 0;
+	pro = 190
+	for i = 1, 10,1 do
+		sum = initNo + sum
+		net = pro - sum
+		print(i..": "..initNo.." calc: "..sum.." GetSum: "..pro.." Net: "..net)
+		initNo = initNo * 2
+		pro = pro * 2
+	end
+	print("sum of money: "..sum)
+end
+
+if arg[1] == nil then
+	arg[1] = 5
+end
+
+calcStep(arg[1])
